6014e8f87611c6753e266f6d7219511583bc1b49
Greg Smith <greg.smith3492@gmail.com>
2018-06-18 16:07:43 -0400

    change number sims to 3

--- Rec_Engine/text_rec.py
+++ Rec_Engine/text_rec.py
@@ -1,6 +1,5 @@
 
 import pandas as pd
-from flask import current_app
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel
 
@@ -21,7 +20,7 @@ class RecommendationSystem(object):
                              min_df=0, stop_words='english')
 
         trans_matrix = model.fit_transform(df['text'])
-        similarities = linear_kernel(tf_matrix, tf_matrix)
+        similarities = linear_kernel(trans_matrix, trans_matrix)
         return similarities
 
     def generate_similarity(self, matrix):
@@ -29,18 +28,15 @@ class RecommendationSystem(object):
         with open('similar_text_matrix.csv', 'w+') as similarity_matrix:
             similarity_matrix.write('self_id,id1,id2,id3,id4,id5,id6,id7,id8\n')
             for index, row in df.iterrows():
-                similar_indices = matrix[index].argsort()[:-10:-1]
+                similar_indices = matrix[index].argsort()[:-4:-1]
                 similar_items = [(matrix[index][i], df['item_id'][i]) for i in similar_indices]
                 new_items = sum(similar_items[:], ())
                 # Unpack the new_items into the item names we're writing to csv
                 # In this basic version we're not keeping the actual values
-                self_score, self_id, score1, id1, score2, id2, score3, id3, score4, \
-                id4, score5, id5, score6, id6, score7, id7, score8, id8 = new_items
+                self_score, self_id, score1, id1, score2, id2, score3, id3 = new_items
                 #item_name = df[(df['item_id'] == self_id)][:1]['item_name'].item()
-                new_line = "{0},{1},{2},{3},{4},{5},{6},{7},{8}".format(
-                    self_id,id1,id2,id3,id4,id5,id6,id7,id8)
-
-                similarity_matrix.write('{new_line}\n'.format(new_line=new_line))
+                new_line = "{0},{1},{2},{3}".format(self_id,id1,id2,id3)
+                similarity_matrix.write('{}\n'.format(new_line))
 
         
         return new_items, new_line
